9bd231f46d11987794b051f2b7b1e4038ec6c280
MatiasDevA <matias.pizarro147@gmail.com>
2023-05-15 21:32:40 -0300

    docker

--- demo/src/main/java/proyectofinalizado/demo/security/MainSecurity.java
+++ demo/src/main/java/proyectofinalizado/demo/security/MainSecurity.java
@@ -56,7 +56,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/**", "/publicacion/**" , "/comentario/**", "/experiencia/**", "/educacion/**", "/skill/**", "/proyecto/**").permitAll()
+                .antMatchers("/auth/**", "/publicacion/**" , "/comentario/**", "/experiencia/**", "/educacion/**", "/skill/**", "/proyecto/**", "/all", "/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
